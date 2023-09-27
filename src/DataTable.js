8119a4aa9e50baeba532428bb885d305d6d82edd
sharmrb <57459468+sharmrb@users.noreply.github.com>
2023-09-27 01:37:55 -0400

    Added table for both sma5 and 30

--- src/DataTable.js
+++ src/DataTable.js
@@ -1,35 +1,35 @@
-import React from 'react';
-
 const DataTable = ({ data }) => {
-    if (!data || !data.sma5 || !data.time) {
+    if (!data || !data.sma5 || !data.sma30 || !data.time5 || !data.time30) {
       return <p>No data available.</p>;
     }
-
-  const { sma5, time } = data;
-
-  return (
-    <div>
-      <h2>SMA Data Table</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Time</th>
-            <th>5-Minute SMA</th>
-            <th>30-Minute SMA</th>
-          </tr>
-        </thead>
-        <tbody>
-          {time.map((timestamp, index) => (
-            <tr key={index}>
-              <td>{timestamp}</td>
-              <td>{sma5[index]}</td>
-              {/* Display 30-minute SMA data here */}
+  
+    const { sma5, sma30, time5, time30 } = data;
+  
+    return (
+      <div>
+        <h2>SMA Data Table</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Time (5-Min)</th>
+              <th>5-Minute SMA</th>
+              <th>Time (30-Min)</th>
+              <th>30-Minute SMA</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
+          </thead>
+          <tbody>
+            {time5.map((timestamp, index) => (
+              <tr key={index}>
+                <td>{timestamp}</td>
+                <td>{sma5[index]}</td>
+                <td>{time30[index]}</td>
+                <td>{sma30[index]}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  };
 
-export default DataTable;
+    export default DataTable;
--- src/StockInputPage.js
+++ src/StockInputPage.js
@@ -1,39 +1,75 @@
 // StockInputPage.js
-
+// ApIKey = 60282fcda5b847378bfa7c03f57d91ec
 import React, { useState } from 'react';
 import DataTable from './DataTable';
 
+
 const StockInputPage = () => {
   const [symbol, setSymbol] = useState('');
-  const [data, setData] = useState({ sma5: [], sma30: [], time: [] });
-
+  const [data, setData] = useState({ sma5: [], sma30: [], time5: [], time30: [] });
+const[fetchingActive, setfetchingActive] = useState(false);
+const fetchingDuration= 2*60*1000        //2 mins CAN BE CHANGED
   const handleSymbolChange = (e) => {
     setSymbol(e.target.value);
   };
 
+
+
+
   const handleStartButtonClick = async () => {
     try {
-      const symbol = 'AAPL'; // Replace with the symbol you want to fetch
-      const interval5 = '5min'; // 5-minute interval
-      const interval30 = '30min'; // 30-minute interval
-      const apiKey = '60282fcda5b847378bfa7c03f57d91ec'; // Replace with your actual API key
+        const apiKey = '60282fcda5b847378bfa7c03f57d91ec'; // Replace with your actual API key
   
-     // Fetch 5-minute SMA data
-const apiData5 = await fetchData(symbol, interval5, apiKey);
+        // Fetch 5-minute SMA data
+        const { sma: sma5, time: time5 } = await fetchData(symbol, '5min', apiKey);
+  
+        // Fetch 30-minute SMA data
+        const { sma: sma30, time: time30 } = await fetchData(symbol, '30min', apiKey);
+  
+        // Update the state with both 5-minute and 30-minute SMA data
+        setData({ sma5, sma30, time5, time30 });
+      } catch (error) {
+        // Handle errors
+      }
+      
+  
 
-// Fetch 30-minute SMA data
-const apiData30 = await fetchData(symbol, interval30, apiKey);
+    };
 
-// Update the state with the fetched data
-setData({ sma5: apiData5.sma5, sma30: apiData30.sma, time: apiData5.time }); // Corrected 'sma30' here
 
+
+const fetchData = async (symbol, interval, apiKey) => {
+    try {
+      const apiUrl = `https://api.twelvedata.com/sma?symbol=${symbol}&interval=${interval}&apikey=${apiKey}&outputsize=1`;
+      const apiResponse = await fetch(apiUrl, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!apiResponse.ok) {
+        throw new Error('Network response not ok');
+      }
+
+      const apiData = await apiResponse.json();
+      console.log('API Response:', apiData);
+
+      if (!apiData.values || !Array.isArray(apiData.values)) {
+        throw new Error('API response invalid data');
+      }
+
+      // Extract SMA values and time
+      const sma = apiData.values.map((item) => parseFloat(item.sma));
+      const time = apiData.values.map((item) => item.datetime);
+
+      return { sma, time };
     } catch (error) {
-      // Handle errors
+      console.error('Error fetching data:', error);
+      throw error;
     }
   };
   
-  
-
   return (
     <div>
       <h1>Day Trading App</h1>
@@ -50,49 +86,15 @@ setData({ sma5: apiData5.sma5, sma30: apiData30.sma, time: apiData5.time }); // 
         <button onClick={handleStartButtonClick}>Start</button>
       </div>
       {data && (
-        <div>
+
           <h2>Fetched Data</h2>
-          <pre>{JSON.stringify(data, null, 2)}</pre>
-        </div>
+         
+
       )}
       <DataTable data={data} />
     </div>
   );
 };
-
-async function fetchData(symbol, interval, apiKey) {
-    try {
-      const apiUrl = `https://api.twelvedata.com/sma?symbol=${symbol}&interval=${interval}&apikey=${apiKey}`;
-      const apiResponse = await fetch(apiUrl, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      
-
-  
-      if (!apiResponse.ok) {
-        throw new Error('Network response was not ok');
-      }
-  
-      const apiData = await apiResponse.json();
-      console.log(apiData);
-      if (!apiData.values || !Array.isArray(apiData.values)) {
-        throw new Error('API response is missing or has invalid data');
-      }
-  
-      // Extract SMA values and time
-      const sma5 = apiData.values.map((item) => parseFloat(item.sma));
-      const time = apiData.values.map((item) => item.datetime);
-      
-      return { sma5, time };
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      throw error;
-    }
-  }
-  
   
 
 export default StockInputPage;
